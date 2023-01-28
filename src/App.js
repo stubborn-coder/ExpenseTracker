d13d57223f07fcc67b00ee27960672405e981826
Muhammad Bilal <mbbilal0612@gmail.com>
2023-01-28 13:38:54 +0500

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 
+import { Header } from './components/Header.jsx';
+
 function App() {
   return (
     <div className="App">
       Hello World
+      <Header/>
+      
     </div>
   );
 }
